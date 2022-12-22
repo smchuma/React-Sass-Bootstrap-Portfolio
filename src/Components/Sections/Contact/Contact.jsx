0d994922bb2411885d9fde003c626267fc7bdef7
Samora Laurent <laurentsamora6@gmail.com>
2022-12-23 01:07:54 +0300

    doing some sass cleanups

--- src/Components/Sections/Contact/Contact.jsx
+++ src/Components/Sections/Contact/Contact.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import ScrollAnimation from "react-animate-on-scroll";
 import { PageTitle } from "../../../Components";
 
 function Contact() {
--- src/Components/Sections/Projects/Projects.jsx
+++ src/Components/Sections/Projects/Projects.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Project, PageTitle } from "../../../Components";
 import { projectData } from "./data";
 
