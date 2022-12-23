266e02c4d302d3158afc017e543add8f123671b8
Samora Laurent <laurentsamora6@gmail.com>
2022-12-23 04:58:45 +0300

    added a preloader

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "animate.css/animate.min.css";
+import HashLoader from "react-spinners/HashLoader";
 import { Home } from "./Pages";
 import "./App.scss";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      {loading ? (
+        <div className="loader">
+          <HashLoader color={"#b9bdcf"} loading={loading} size={100} />
+        </div>
+      ) : (
+        <Home />
+      )}
+    </>
   );
 };
 
